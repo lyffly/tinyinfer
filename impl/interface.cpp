3868892774b664310630dfdc959a185ba0e884d3
liuyunfei <lyfflyl@gmail.com>
2024-07-16 23:25:50 +0800

    add submoudle pybind11

---
+++ impl/interface.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(kernels, m) {
+    m.def("add", &add );
+    m.def("subtract", [](int i, int j) { return i - j; });
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
