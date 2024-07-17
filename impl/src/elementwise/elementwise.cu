546a3efdb4a393e5996fb19f7fde58a9414613e1
liuyunfei <lyfflyl@gmail.com>
2024-07-17 23:26:36 +0800

    add setup.py

--- impl/src/elementwise/elementwise.cu
+++ impl/src/elementwise/elementwise.cu
@@ -15,7 +15,7 @@ __global__ void elementwise_fp_cuda(T *in0, T *in1, T *out, int length) {
 
 bool elementwise(int64_t in_ptr0, int64_t in_ptr1, int64_t out_ptr,
                 std::vector<int> in_shape0, std::vector<int> in_shape1,
-                std::vector<int> out_shape, std::string dtype, std::string layout) {
+                std::vector<int> out_shape, std::string dtype, std::string layout, std::string optype) {
 
     int block_size = 512;
     int length = 1;
